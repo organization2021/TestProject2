0bc3b5261b47dfcc67614d8b532e08694cab7bc2
yulyamo <77944236+yulyamo@users.noreply.github.com>
2021-11-16 23:23:45 -0600

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/*slider box code*/
+$(document).ready(function(){
+  $('.slider').bxSlider({
+    captions: true
+  });
+});
+/*code for the sign up page*/
+document.getElementById("news-verification").onclick = function() {
+    myFunction();
+}
+function myFunction() {
+  var checkBox = document.getElementById("news-verification");
+  var text = document.getElementById("text");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+document.getElementById("theForm").onsubmit = function() {
+  submitFunction();
+}
+
+function submitFunction() {
+  alert("The confirmation was sent to provided email address. Check your inbox and verify the email.");
+}
+/* end of code for the sign up page*/
+
